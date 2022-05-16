f0e10a053b20bb2c90179bd323d19772df56c521
Duilio Murai <duiliomurai@gmail.com>
2022-05-16 12:33:01 -0300

    Comecando implementar o tabuleiro

---
+++ Xadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿namespace Xadrez.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+
+        
+    }
+}
